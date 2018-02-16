6320bd9e2a5f1bc821b87a42c5e116dfe3a3f778
Markus Huggler <markus.huggler@ecologic.ch>
2018-02-16 17:03:30 +0100

    Adapted to directus

--- plugin.js
+++ plugin.js
@@ -1,36 +1,31 @@
-tinymce.PluginManager.add('ecoimages', function (editor, url) {
-  // Add a button that opens a window
-  editor.addButton('eco-images', {
-    text: 'EcoImages',
-    icon: 'image',
-    onclick: new Dialog(editor).open
-  });
+tinymce.PluginManager.add('image', function (editor, url) {
 
-  // Adds a menu item to the tools menu
-  editor.addMenuItem('eco-images', {
-    text: 'EcoImages',
-    context: 'insert',
-    onclick: new Dialog(editor).open
-  });
+  var getImageList = function (editor) {
+    return editor.getParam('image_list', false);
+  };
 
-  // Include Plugin Css
-  editor.on('init', function () {
-    var cssLink = editor.dom.create('link', {
-      rel: 'stylesheet',
-      href: url + '/styles.css'
-    });
-    document.getElementsByTagName('head')[0].appendChild(cssLink);
-  });
+  var createImageList = function (editor, callback) {
+    var imageList = ecoMapping.getImageList(editor);
+    if (typeof imageList === 'string') {
+      XHR.send({
+        url: imageList,
+        success: function (text) {
+          callback(JSON.parse(text));
+        }
+      });
+    } else if (typeof imageList === 'function') {
+      imageList(callback);
+    } else {
+      callback(imageList);
+    }
+  };
 
-
-  function createHtml() {
-
-    var imageList = editor.getParam('image_list', false);
-
+  var createHtml = function (imageList) {
     var contentDiv = document.createElement('div');
     contentDiv.setAttribute('class', 'mce-eco-panel-content');
     for (var imageIndex = 0; imageIndex < imageList.length; imageIndex++) {
-      var title = imageList[imageIndex].title;
+      var title = imageList[imageIndex].text;
+      console.log(imageList[imageIndex]);
       var imageDiv = document.createElement('div');
       imageDiv.setAttribute('class', 'mce-eco-image-cell');
       var image = document.createElement('img');
@@ -44,18 +39,55 @@ tinymce.PluginManager.add('ecoimages', function (editor, url) {
       contentDiv.appendChild(imageDiv);
     }
     return contentDiv.outerHTML;
-  }
+  };
+
+  var register = function (editor) {
+    // Add a button that opens a window
+    editor.addButton('image', {
+      text: '',
+      icon: 'image',
+      onclick: new Dialog(editor).open
+    });
+
+    // Adds a menu item to the tools menu
+    editor.addMenuItem('image', {
+      text: 'EcoImages',
+      context: 'insert',
+      onclick: new Dialog(editor).open
+    });
+
+    // Include Plugin Css
+    editor.on('init', function () {
+      var cssLink = editor.dom.create('link', {
+        rel: 'stylesheet',
+        href: url + '/styles.css'
+      });
+      document.getElementsByTagName('head')[0].appendChild(cssLink);
+    });
+  };
+
+  var ecoMapping = {
+    getImageList: getImageList,
+    createImageList: createImageList,
+    createHtml: createHtml,
+    register: register
+  };
 
   function Dialog(editor) {
+
     function open() {
+      ecoMapping.createImageList(editor, showDialog);
+    }
+
+    function showDialog(imageList) {
       var win = editor.windowManager.open({
         title: 'EcoImages',
         autoScroll: true,
-        width: 700,
+        width: 900,
         height: 500,
         body: [
           {
-            type: 'container', html: createHtml()
+            type: 'container', html: ecoMapping.createHtml(imageList)
           }
         ],
         buttons: [{
@@ -77,6 +109,8 @@ tinymce.PluginManager.add('ecoimages', function (editor, url) {
 
     return {open: open}
   }
+
+  register(editor);
 
   return {
     getMetadata: function () {
